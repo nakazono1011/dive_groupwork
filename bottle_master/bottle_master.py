364a1c000792f0cebcb2a9df67e3e3125a0ba565
satoshi30 <ikeda.satoshi30@gmail.com>
2020-10-12 20:55:56 +0900

    fix ikeda

---
+++ bottle_master/bottle_master.py
@@ -0,0 +1,35 @@
+
+import os
+import csv
+
+# bottle_masterを読み込みを辞書型を取得する
+def bottle_master_dict():
+    bottle_master_dict = {}
+    # csvファイルを読み込む
+    cwd_path = os.getcwd()
+    file_path = os.path.join(cwd_path, 'bottle_master/bottle_master.csv')
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        for bottle_name, price in reader:
+            bottle_master_dict[bottle_name] = int(price)
+
+    return bottle_master_dict
+
+# 学習データが格納されているディレクトリのパスとラベルの辞書を取得する
+def bottle_label_dirpapth():
+    data_dir_path = []
+    label_dict = {}
+    label = 0
+    # csvファイルを読み込む
+    cwd_path = os.getcwd()
+    file_path = os.path.join(cwd_path, 'bottle_master/bottle_master.csv')
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        for bottle_name, _ in reader:
+            # pathの取得、リストへ
+            path = os.path.join(cwd_path, 'shutter2image', bottle_name)
+            data_dir_path.append(path)
+            # ラベルを辞書へ記録
+            label_dict[bottle_name] = label
+            label += 1
+    return data_dir_path, label_dict
