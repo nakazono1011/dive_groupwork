2fae6859dd7da2c9fc65376daac7573b34c4f7fa
satoshi30 <ikeda.satoshi30@gmail.com>
2020-10-14 12:45:29 +0900

    fic function

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import pygame.mixer
 import numpy as np
 import picamera
 from PIL import Image
-from time import sleep
+from time import sleep, perf_counter
 
 from bottle_master import bottle_master
 from shutter2image import shutter2image
@@ -16,103 +16,158 @@ import datetime
 import argparse
 
 parser = argparse.ArgumentParser(description='model_path')
+# modelが保存されているpathを引数で指定してください
 parser.add_argument('--model_path', default=None, help="path to saved_model")
-parser.add_argument('--debug_without_model', default=None, help="not_predict")
-
-# def shutter():
-#     photofile = open(photo_filename, 'wb')
-#     print(photofile)
-#
-#     # pi camera 用のライブラリーを使用して、画像を取得
-#     with picamera.PiCamera() as camera:
-#         #camera.resolution = (640,480)
-#         camera.resolution = (300,400)
-#         camera.start_preview()
-#         sleep(1.000)
-#         camera.capture(photofile)
+# デフォルトで引数を指定しない場合はmodelがloadされ予測が実行されます debugを行う場合はFalse
+parser.add_argument('--predict_mode', default=True, help="model is loaded and prediction is excuted.")
+# 画像を保存するかどうか、保存する場合はpathを指定
+parser.add_argument('--image_save', default=None, help="input path if you want to save images.")
 
 if __name__ == '__main__':
-    #モデルが保存されているパスを読み込み
+    # 引数を読み込み
     args = parser.parse_args()
-    if args.model_path is None:
+    # predictモードの場合
+    if args.predict_mode == True and args.model_path is None:
         print('--model_pathを指定してください')
     else:
-        #画像を保存するディレクトリを作成
-        os.makedirs('check_image', exist_ok=True)
-        # モデル+重みを読込み
-        if args.debug_without_model is None:
-            self_model = load_model(args.model_path)
-        else:
-            self_model = 0
+        #画像を保存する場合、ディレクトリを作成
+        if args.image_save:
+            try:
+                os.makedirs(args.image_save, exist_ok=True)
+            except:
+                print('読み取った画像を保存するpathが正しく指定されていません')
 
-        #商品価格
+        # モデル+重みを読込み
+        if args.predict_mode is True:
+            self_model = load_model(args.model_path)
+
+        #商品価格等の情報をbottle_masterから読み込み
         name_price_dict = bottle_master.bottle_master_dict()
         _, label_dict = bottle_master.bottle_label_dirpath()
         bottle_label = {}
         for key, value in label_dict.items():
             bottle_label[value] = key
 
-        # 音声ファイル初期化
-        # pygame.mixer.init()
-        # pygame.mixer.music.load("Cash_Register-Beep01-1.mp3")
-
-        # # 正解ラベル
-        # label = ['cocacola-peach', 'ilohas', 'kuchidoke-momo', 'o-iocha', 'pocari-sweat']
-        # # 商品価格
-        # money = {'cocacola-peach':110, 'ilohas':120, 'kuchidoke-momo':130, 'o-iocha':140, 'pocari-sweat':150}
-
+        language_dict = {'k':'Kinyarwanda', 'e':'English', 'f':'French', 'j':'Japanese'}
         while True:
             money_sum = 0
-            while True:
-                key = input('商品を指定の位置に設置し「Enter」を押して下さい')
-                dt_now = datetime.datetime.now()
-                file_name = 'check' + '_' + dt_now.strftime('%Y%m%d%H%M%S') + '.jpg'
-                save_path = os.path.join('check_image', file_name)
-                # 写真撮影
-                shutter2image.shutter(save_path)
+            print('which language? Please select a language and input a key!!')
+            language = input('Kinyarwanda :「k」,English :「e」,French :「f」,Japanese :「j]')
+            if language not in ['k', 'e', 'f', 'j']:
+                print('Please input correctly')
+                pass
+            else:
+                print('Language_mode : {}'.format(language_dict[language]))
 
-                # # 音声再生
-                # pygame.mixer.music.play(1)
-                # sleep(1)
-                # # 再生の終了
-                # pygame.mixer.music.stop()
+                while True:
+                    input('商品を指定の位置に設置し「Enter」を押して下さい')
 
-                # 画像をモデルの入力用に加工
-                img = Image.open(save_path)
-                #img = Image.open("./0.jpg")
-                img = img.resize((224, 224))
-                img_array = img_to_array(img)
-                img_array = img_array.astype('float32')/255.0
-                img_array = img_array.reshape((1,224,224,3))
+                    # 時間を図る
+                    time_start = perf_counter()
 
-                if self_model == 0:
-                    print('商品が読み取られました')
-                    print('array_shape : {}'.format(img_array.shape))
-                    key = input('読み取られた商品が正しい場合は「y」、誤っていた場合は「n」を押してください')
-                    if key == 'y':
-                        money_sum += 100
-                        print("小計", money_sum)
-                        key = input('続けて商品をスキャンする場合は「y」,会計する場合は「Enter」を押して下さい')
-                        if key != 'y':
-                            print('合計 : {}'.format(money_sum))
-                            print("Thank you for your purchase!!")
-                            break
-                    elif key == 'n':
-                        print('商品を指定の位置に置き直してください')
-                else:
-                    # predict
-                    img_pred = self_model.predict(img_array)
-                    print("debug:",img_pred)
-                    name = bottle_label[np.argmax(img_pred)]
-                    print('商品 : {} 値段 : {} が読み取られました'.format(name, name_price_dict[name]))
-                    key = input('読み取られた商品が正しい場合は「y」、誤っていた場合は「n」を押してください')
-                    if key == 'y':
-                        money_sum += name_price_dict[name]
-                        print("小計", money_sum)
-                        key = input('続けて商品をスキャンする場合は「y」,会計する場合は「Enter」を押して下さい')
-                        if key != 'y':
-                            print('合計 : {}'.format(money_sum))
-                            print("Thank you for your purchase!!")
-                            break
-                    elif key == 'n':
-                        print('商品を指定の位置に置き直してください')
+                    # 写真撮影した日時でファイル名を作成
+                    dt_now = datetime.datetime.now()
+                    file_name = 'checked' + '_' + dt_now.strftime('%Y%m%d%H%M%S') + '.jpg'
+
+                    # 画像をsaveするpathを作成
+                    if args.image_save:
+                        save_path = os.path.join(args.image_save, file_name)
+                    else:
+                        save_path = os.path.join(os.getcwd(), file_name)
+
+                    # 写真撮影してjpgをディレクトリへ保管
+                    # ローカルデバッグ
+                    # shutter2image.shutter(save_path)
+
+                    # # 音声再生
+                    # pygame.mixer.music.play(1)
+                    # sleep(1)
+                    # # 再生の終了
+                    # pygame.mixer.music.stop()
+
+                    # 画像をモデルの入力用に加工
+
+                    img = Image.open(save_path)
+                    # ローカルデバッグ
+                    # img = Image.open('/Users/ikeda/Desktop/IMG_0819.jpg')
+                    # 画像保存のpathが指定されていない場合は消去
+                    if args.image_save == None:
+                        os.remove(save_path)
+
+                    #img = Image.open("./0.jpg")
+                    img = img.resize((224, 224))
+                    img_array = img_to_array(img)
+                    img_array = img_array.astype('float32')/255.0
+                    img_array = img_array.reshape((1,224,224,3))
+
+                    # predict_modeによって分岐
+                    # modelが読み込まれてなく、流れのみ再現
+                    if args.predict_mode == 'False':
+                        # 時間計測
+                        time_end = perf_counter()
+                        print('商品を読み取るまで {}[s] 経過しました'.format(time_end-time_start))
+                        print('商品 : not predict が読み取られました')
+                        print('img_array_shape : {}'.format(img_array.shape))
+
+                        while True:
+                            key = input('読み取られた商品が正しい場合は「y」、誤っていた場合は「n」を押してください')
+                            if key != 'y' and key != 'n':
+                                print('正しく入力されていません')
+                            else:
+                                break
+
+                        if key == 'y':
+                            money_sum += 100
+                            print("小計", money_sum)
+
+                            while True:
+                                key = input('続けて商品をスキャンする場合は「y」,終了場合は「f」を押して下さい')
+                                if key != 'y' and key != 'f':
+                                    print('正しく入力されていません')
+                                else:
+                                    break
+                            if key == 'f':
+                                print('合計 : {}'.format(money_sum))
+                                print("Thank you for your purchase!!")
+                                print()
+                                break
+                            elif key == 'n':
+                                print('次の商品を指定の位置に置いてください')
+
+                    # 読み込んだmodelでpridictを実行
+                    else:
+                        img_pred = self_model.predict(img_array)
+                        print("debug:",img_pred)
+                        name = bottle_label[np.argmax(img_pred)]
+
+                        # 時間計測
+                        time_end = perf_counter()
+                        print('商品を読み取るまで {}[s] 経過しました'.format(time_end-time_start))
+
+                        print('商品 : {} 値段 : {} が読み取られました'.format(name, name_price_dict[name]))
+
+                        while True:
+                            key = input('読み取られた商品が正しい場合は「y」、誤っていた場合は「n」を押してください')
+                            if key != 'y' and key != 'n':
+                                print('正しく入力されていません')
+                            else:
+                                break
+
+                        if key == 'y':
+                            money_sum += name_price_dict[name]
+                            print("小計", money_sum)
+
+                            while True:
+                                key = input('続けて商品をスキャンする場合は「y」,終了場合は「f」を押して下さい')
+                                if key != 'y' and key != 'f':
+                                    print('正しく入力されていません')
+                                else:
+                                    break
+
+                            if key == 'f':
+                                print('合計 : {}'.format(money_sum))
+                                print("Thank you for your purchase!!")
+                                print()
+                                break
+                            elif key == 'n':
+                                print('次の商品を指定の位置に置いてください')
